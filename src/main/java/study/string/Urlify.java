f454558c166fe60d9e70115a0952dde4b59719c0
Vu Do <vu.do@shizzle.co>
2017-11-26 10:13:08 +0700

    add urlify

---
+++ src/main/java/study/string/Urlify.java
@@ -0,0 +1,32 @@
+package study.string;
+
+import java.util.Scanner;
+
+/*
+    1.3 urlify a string, replace spaces with %20
+
+    Hint: check Space, Case Sensitive
+    O(n), S(n)
+ */
+public class Urlify {
+    public static String urlify(String str, int length) {
+        StringBuilder sb = new StringBuilder();
+        for (int i = 0; i < str.length(); i++) {
+            if (str.charAt(i) == ' ') {
+                if (i < str.length() - 1 && str.charAt(i + 1) != ' ')
+                    sb.append("%20");
+                continue;
+            } else {
+                sb.append(str.charAt(i));
+            }
+        }
+
+        return sb.toString();
+    }
+
+    public static void main(String[] args) {
+        Scanner in = new Scanner(System.in);
+        String a = in.nextLine();
+        System.out.println(urlify(a, a.length()));
+    }
+}
