772d9632e640112ae4a0ab0e386c3c4cdf2a71ad
Vu Do <dohaivu@gmail.com>
2018-03-26 14:44:22 +0700

    implement camel case

--- src/main/java/study/string/Anagram.java
+++ src/main/java/study/string/Anagram.java
@@ -8,7 +8,23 @@ import java.util.regex.*;
 import java.util.stream.*;
 
 /**
- * two strings to be anagrams of each other if the first string's letters can be rearranged to form the second string
+Alice is taking a cryptography class and finding anagrams to be very useful. We consider two strings to be anagrams of each other if the first string's letters can be rearranged to form the second string. In other words, both strings must contain the same exact letters in the same exact frequency For example, bacdc and dcbac are anagrams, but bacdc and dcbad are not.
+
+Alice decides on an encryption scheme involving two large strings where encryption is dependent on the minimum number of character deletions required to make the two strings anagrams. Can you help her find this number?
+
+Given two strings, a and b, that may or may not be of the same length, determine the minimum number of character deletions required to make a and b anagrams. Any characters can be deleted from either of the strings.
+
+- Input
+cde
+abc
+- Output
+4
+- Explanation
+We delete the following characters from our two strings to turn them into anagrams of each other:
+
+Remove d and e from cde to get c.
+Remove a and b from abc to get c.
+We must delete 4 characters to make both strings anagrams, so we print 4 on a new line.
  */
 public class Anagram {
     /**
--- src/main/java/study/string/CamelCase.java
+++ src/main/java/study/string/CamelCase.java
@@ -1,25 +1,40 @@
 package study.string;
 
+import java.util.Arrays;
 import java.util.Scanner;
 
 /**
  * Make a string "This is a camel case" to "ThisIsACamelCase"
+ * Use 2 index: read, write
+ * O(n)
  */
 public class CamelCase {
     public static String makeCamelCase(String a) {
-        char[] charArray = a.toCharArray();
-        for (int i = 0; i < charArray.length; i++) {
-            if (charArray[i] == ' ') {
+        if (a.length() == 0) return a;
 
+        char[] charArray = a.toCharArray();
+        int j = 0;
+        boolean isSpace = true;
+        for (int i = 0; i < charArray.length; i++) {
+            if (charArray[i] != ' ') {
+                if (isSpace)
+                    charArray[j++] = Character.toUpperCase(charArray[i]);
+                else
+                    charArray[j++] = charArray[i];
+
+                isSpace = false;
+            } else {
+                isSpace = true;
             }
         }
 
-        return "";
+        return new String(Arrays.copyOf(charArray, j));
     }
 
     public static void main(String[] args) {
         Scanner in = new Scanner(System.in);
         String a = in.nextLine();
         System.out.println(makeCamelCase(a));
+        in.close();
     }
 }
