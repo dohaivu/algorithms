05b3a608092287bcdb10b0adc5cb611a0b2afeaf
Vu Do <dohaivu@gmail.com>
2018-03-26 15:11:25 +0700

    update isunique

--- src/main/java/study/string/IsUnique.java
+++ src/main/java/study/string/IsUnique.java
@@ -4,9 +4,20 @@ import java.util.Scanner;
 
 /*
     1.1 A string has all unique character
+    - Input
+    abca
+    abc
+    - Output
+    false
+    true
+    - Explanation
+
+    Use array hold the appearance of characters
+    O(n)
  */
 public class IsUnique {
      public static boolean isUniqueChars(String str) {
+        if (str.length() == 0) return true;
         if (str.length() > 128) return false;
 
         boolean[] char_set = new boolean[128];
@@ -23,5 +34,7 @@ public class IsUnique {
         Scanner in = new Scanner(System.in);
         String a = in.next();
         System.out.println(isUniqueChars(a));
+
+        in.close();
     }
 }
